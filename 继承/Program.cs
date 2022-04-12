836952f793ed8d2c09c129bbc237aeed2cef14ba
笑一个吧 <1242615901@qq.com>
2022-04-12 21:22:56 +0800

    1

--- 继承/Program.cs
+++ 继承/Program.cs
@@ -1,21 +1,25 @@
 ﻿global using global::Newtonsoft.Json.Linq;
 
-
+/*继承是面向对象程序设计中最重要的概念之一。
+ * 继承允许我们根据一个类来定义另一个类，这使得创建和维护应用程序变得更容易。同时也有利于重用代码和节省开发时间。
+ * 当创建一个类时，程序员不需要完全重新编写新的数据成员和成员函数，只需要设计一个新的类，继承了已有的类的成员即可。
+ * 这个已有的类被称为的基类，这个新的类被称为派生类。*/
 {
-    /*
-     * C#不支持多继承，如果想实现需要通过接口。
-     * 
-     * 
-     */
-    长方体体积 rectangle = new 长方体体积(10);
-    rectangle.Width = 10;
-    rectangle.Height = 10;
-    rectangle.GetArea();
-    rectangle.Display_vir();
-    rectangle.Didplay_Abs();
+    //长方体体积 rectangle = new 长方体体积(10);
+    //rectangle.Width = 10;
+    //rectangle.Height = 10;
+    //rectangle.GetArea();
+    //rectangle.Display_vir();
+    //rectangle.Didplay_Abs();
+
+    ParentClass par = new ChildClass1();
+    par.ParVirMethod(); //结果："子类1的方法"，调用子类的方法，实现了多态
+
+    par = new ChildClass2();
+    par.ParVirMethod(); //结果："父类的方法"，调用父类的方法，没有实现多态  
 }
 
-
+#region 继承基本，基类有参构造，抽象，虚拟
 abstract class Shape
 {
 
@@ -34,6 +38,7 @@ abstract class Shape
 
 
 }
+
 class Rectangle : Shape
 {
     public Rectangle(int a) : base(a)
@@ -60,7 +65,7 @@ class Rectangle : Shape
 
 class 长方体体积 : Rectangle
 {
-    public 长方体体积(int a):base(a)
+    public 长方体体积(int a) : base(a)
     {
         Console.WriteLine($"长方体体积{a}");
     }
@@ -71,6 +76,48 @@ class 长方体体积 : Rectangle
     public override void Didplay_Abs()
     {
         base.Didplay_Abs();
+        Console.WriteLine();
     }
 
 }
+#endregion
+
+/// <summary>  
+/// 父类  
+/// </summary>  
+public class ParentClass
+{
+    public virtual void ParVirMethod()
+    {
+        Console.WriteLine("父类的方法...");
+    }
+}
+
+/// <summary>  
+/// 子类1  
+/// </summary>  
+public class ChildClass1 : ParentClass
+{
+    public override void ParVirMethod()
+    {
+        Console.WriteLine("子类1的方法...");
+    }
+}
+
+/// <summary>  
+/// 子类2  
+/// </summary>  
+public class ChildClass2 : ParentClass
+{
+    public new void ParVirMethod()
+    {
+        Console.WriteLine("子类2的方法...");
+    }
+
+    public void Test()
+    {
+        Console.WriteLine("子类2的其他方法...");
+    }
+}
+
+
